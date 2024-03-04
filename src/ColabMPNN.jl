a9df7543aa7b37f125dbdef91434a34b0b51e188
anton083 <anton.oresten42@gmail.com>
2024-03-04 17:47:21 +0100

    Add mpnn_sample

--- src/ColabMPNN.jl
+++ src/ColabMPNN.jl
@@ -1,5 +1,23 @@
 module ColabMPNN
 
-# Write your package code here.
+export mpnn_sample
+
+using PyCall, Conda
+
+const colabdesign = PyNULL()
+const mpnn_model = PyNULL()
+
+function __init__()
+    Conda.pip_interop(true)
+    Conda.pip("install", "git+https://github.com/sokrypton/ColabDesign.git@v1.1.1")
+    copy!(colabdesign, pyimport("colabdesign"))
+    copy!(mpnn_model, colabdesign.mk_mpnn_model())
+end
+
+function mpnn_sample(path::String, temp::Float64, chain="A")
+    mpnn_model.prep_inputs(pdb_filename=path, chain=chain)
+    samples = mpnn_model.sample_parallel(temperature=temp)
+    return String.(collect(samples["seq"]))
+end
 
 end
